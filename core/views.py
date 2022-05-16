266f2ecd271c2d71ae3e9eff4075f9172564e497
pochii <johnmwanikig2000@gmail.com>
2022-05-16 10:06:52 +0300

    second commit

--- core/views.py
+++ core/views.py
@@ -9,6 +9,8 @@ from django.views.generic import TemplateView
 from core.forms import AddFixtureForm
 from core.models import Field, Team, Contact, Fixture
 from core.printpdf import html_to_pdf
+from fpdf import FPDF
+from django.http import FileResponse
 
 
 class Homepage(TemplateView):
@@ -121,10 +123,31 @@ class MakeReservation(View):
         return render(request, 'MakeReservation.html')
 
 
-class GeneratePdf(View):
-    def get(self, request, *args, **kwargs):
-        # getting the template
-        pdf = html_to_pdf('MakeReservation.html')
+# class GeneratePdf(View):
+#     def get(self, request, *args, **kwargs):
+#         # getting the template
+#         pdf = html_to_pdf('MakeReservation.html')
+#
+#         # rendering the template
+#         return HttpResponse(pdf, content_type='application/pdf')
 
-        # rendering the template
-        return HttpResponse(pdf, content_type='application/pdf')
+
+def GeneratePdf(request):
+    sales = [
+        {"item": "Keyboard", "amount": "$120,00"},
+        {"item": "Mouse", "amount": "$10,00"},
+        {"item": "House", "amount": "$1 000 000,00"},
+    ]
+    pdf = FPDF('P', 'mm', 'A4')
+    pdf.add_page()
+    pdf.set_font('courier', 'B', 16)
+    pdf.cell(40, 10, 'This is what you have sold this month so far:', 0, 1)
+    pdf.cell(40, 10, '', 0, 1)
+    pdf.set_font('courier', '', 12)
+    pdf.cell(200, 8, f"{'Item'.ljust(30)} {'Amount'.rjust(20)}", 0, 1)
+    pdf.line(10, 30, 150, 30)
+    pdf.line(10, 38, 150, 38)
+    for line in sales:
+        pdf.cell(200, 8, f"{line['item'].ljust(30)} {line['amount'].rjust(20)}", 0, 1)
+    pdf.output('report.pdf', 'F')
+    return FileResponse(open('report.pdf', 'rb'), as_attachment=True, content_type='application/pdf')
